16380dbe6696326c9a3f6e4f76a6d4f0a603421d
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-21 16:09:36 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import {BrowserRouter, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import Users from './Users'
+import About from './About'
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter >
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Route exact path="/" component={Home}  />
+        <Route exact path="/about" component={About}  />
+        <Route exact path="/users" render={() => <Users greetingMessage="Good Morning" />}  />
+      </BrowserRouter>
     );
   }
 }
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Users = () => (
-  <div>
+class Users extends React.Component {
+  render(){
+    return(
+      <div>
     <h2> Users </h2>
-    <p> My awesome Users component </p>
+    <p> {this.props.greetingMessage}, this is my awesome Users component </p>
   </div>
-);
+    )
+  }
+}
 
 export default Users;
